cb88e59d6424654cf6bd45125040a829b57dc2ad
Kishore Ch <kishu.559@gmail.com>
2022-12-21 16:21:13 +0530

    sdsdv

---
+++ swiper/swiper.js
@@ -0,0 +1,12 @@
+ new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
